cee73cdd4f13c7a43ffa49d9510753a10e5c4dd7
Vitalii <v.hrytsiienko@gmail.com>
2016-05-07 08:30:09 +0300

    ad

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,25 +31,19 @@ class Header extends React.Component {
             <NavItem eventKey={2} href='#'>Створити Документ</NavItem>
           </Nav>
           <Nav pullRight>
-            {!user
-           ? <LinkContainer to={{pathname: '/signup'}}>
-             <NavItem eventKey={1} href='#'>Реєстрація</NavItem>
-           </LinkContainer>
-            : null}
-            {!user
-            ? <LinkContainer to={{pathname: '/login'}}>
+            <LinkContainer to={{pathname: '/signup'}}>
+              <NavItem eventKey={1} href='#'>Реєстрація</NavItem>
+            </LinkContainer>
+            <LinkContainer to={{pathname: '/login'}}>
               <NavItem eventKey={2} href='#'>Вхід</NavItem>
             </LinkContainer>
-            : null}
-            {user
-            ? <NavDropdown eventKey={3} title={user.username} id='basic-nav-dropdown'>
+            <NavDropdown eventKey={3} title={user.username || 'Анон'} id='basic-nav-dropdown'>
               <MenuItem eventKey={3.1}>Редагувати профіль</MenuItem>
               <MenuItem eventKey={3.2}>Змінити аватар</MenuItem>
               <MenuItem eventKey={3.3}>Змінити пароль</MenuItem>
               <MenuItem divider />
               <MenuItem eventKey={3.3} onClick={this.handleLogout.bind(this)} >Вихід</MenuItem>
             </NavDropdown>
-            : null}
           </Nav>
         </BootstrapCollapse>
       </Navbar>
